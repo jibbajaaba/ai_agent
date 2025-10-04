706f5e0273ffef52d88c6d19d757885fce160d39
Mark McTague <mmcta275@gmail.com>
2025-10-03 21:20:12 -0400

    setup agent to take questions when prompted and error message if used wrong

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,11 +8,17 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+if len(sys.argv) < 2:
+    print("Usage: uv run main.py <question>")
+    sys.exit(1)
+
+question = sys.argv[1]
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev sucha a great place to learn backend development? use one paragraph maximum."
+    contents=question
 )
 
 print(response.text)
-print("Prompt tokens:", 19)
+print("Prompt tokens:", response.usage_metadata.prompt_token_count)
 print("Response tokens:", response.usage_metadata.candidates_token_count)
