48a45e3f211ee52982d73df69eab8263fad23422
Mark A McTague <mmctaguecoding@gmail.com>
2025-10-07 20:30:39 -0400

    created a messages list that will store the user conversation. Also created a --verbose flag to show more info

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,13 +13,23 @@ if len(sys.argv) < 2:
     print("Usage: uv run main.py <question>")
     sys.exit(1)
 
-question = sys.argv[1]
+user_prompt = sys.argv[1]
+
+messages =  [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=question
+    contents=messages,
 )
 
-print(response.text)
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+
+if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+    print(response.text)
+    print("User prompt:", user_prompt),
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count),
+    print("Response tokens:", response.usage_metadata.candidates_token_count),
+else:
+    print(response.text)
